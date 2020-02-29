8a3f4c00397cacf6d6abd90ce00b784fe9caafae
Lin Let Let Lwin <linletlet2002@gmail.com>
2020-02-29 12:13:42 +0630

    fibonacci.py

---
+++ loop.py
@@ -0,0 +1,19 @@
+-while Loops
+-for Loops
+
+Condition is true, while loop execute a set of statements
+
+x = 1
+while x < 7:
+	print(x)
+	x += 1
+
+
+While loop require variable ready.
+
+x = 1
+while x < 7:
+	print(x)
+	if x == 5:
+		break
+	x += 1
